862f4120ab817d448df6c832fc177371156947ed
xiaoxiao <xiaolanchuan@qq.com>
2025-09-01 06:58:29 +0800

    Update email.js
    bbbot

--- mail-worker/src/email/email.js
+++ mail-worker/src/email/email.js
@@ -171,8 +171,7 @@ export async function email(message, env, ctx) {
 			// xiaoxiao  增加 emailId，messageId
 			const tgMessage = `<b>${params.subject}</b>
 
-<b>messageId::</b>${params.messageId || '0'}
-<b>accountId::</b>${params.accountId || '0'}
+<b>messageId::</b>${params.messageId}
 <b>发件人：</b>${params.name}		&lt;${params.sendEmail}&gt;
 <b>收件人：\u200B</b>${message.to}
 <b>时间：</b>${dayjs.utc(emailRow.createTime).tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm')}
